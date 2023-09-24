6463ba5ece80788c51ba29298751bc00c0d49283
Poral <seynalkim@yahoo.com>
2023-09-24 14:04:46 +0800

    Updated Description in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flashcard'),
