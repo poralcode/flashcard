b209ba3de46debd4cdac78abb31a0424c4984ca8
Poral <seynalkim@yahoo.com>
2023-09-26 00:32:11 +0800

    Updated to Material 3 and.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,55 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flashcard/theme/color_schemes.g.dart';
+import 'package:flashcard/theme/custom_color.g.dart';
 import 'package:flashcard/screens/home_screen.dart';
 import 'package:flashcard/screens/splash_screen.dart';
-
-const String appName = 'Flashcard';
-final String copyright = '${getCurrentYear()} \u00a9 Poralcode';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appName,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: const SplashScreen(),
-      routes: {
-        '/home': (context) => const HomeScreen(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightScheme;
+        ColorScheme darkScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          lightScheme = lightDynamic.harmonized();
+          lightCustomColors = lightCustomColors.harmonized(lightScheme);
+
+          // Repeat for the dark color scheme.
+          darkScheme = darkDynamic.harmonized();
+          darkCustomColors = darkCustomColors.harmonized(darkScheme);
+        } else {
+          // Otherwise, use fallback schemes.
+          lightScheme = lightColorScheme;
+          darkScheme = darkColorScheme;
+        }
+
+        return MaterialApp(
+          title: 'Flashcard', // Capitalize the first letter
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightScheme,
+            extensions: [lightCustomColors],
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkScheme,
+            extensions: [darkCustomColors],
+          ),
+          home: const SplashScreen(),
+          routes: {
+            '/home': (context) => const HomeScreen(),
+          },
+        );
       },
     );
   }
