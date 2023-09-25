74da01726faaa617973dcf84c8069c8666b0c448
Poral <seynalkim@yahoo.com>
2023-09-26 00:36:32 +0800

    Added new screen for adding New Flashcard item.

---
+++ lib/screens/new_flashcard.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+/* NewFlashcard Screen */
+class NewFlashcard extends StatefulWidget {
+  const NewFlashcard({Key? key}) : super(key: key);
+
+  @override
+  _NewFlashcardState createState() => _NewFlashcardState();
+}
+
+class _NewFlashcardState extends State<NewFlashcard> {
+  final titleController = TextEditingController();
+  String? errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    final focusNode = FocusNode();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Flashcard'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            // Centered Container to limit the width and align horizontally
+            Center(
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 400), // Limit the width
+                child: Column(
+                  children: [
+                    // TextField for the title
+                    TextField(
+                      key: Key('title_textfield'), // Assign a unique Key
+                      controller: titleController,
+                      autofocus: true,
+                      maxLength: 150,
+                      focusNode: focusNode,
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                        hintText: 'e.g. Medical Equipments',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        helperText:
+                            'Add a meaningful title for this flashcard.',
+                        border: OutlineInputBorder(),
+                        errorText: errorText,
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            titleController.clear();
+                            setState(() {
+                              errorText = null;
+                            });
+                          },
+                          icon: Icon(Icons.clear),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        validateTitle(value);
+                      },
+                    ),
+                    // Add spacing between TextField and Button
+                    SizedBox(height: 16),
+
+                    // Align the Button to the right
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Check if there's no error before continuing
+                          if (errorText == null) {
+                            // Handle the continue button click
+                            debugPrint("Continue button clicked");
+                          }
+                        },
+                        child: Text("Continue"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void validateTitle(String value) {
+    if (value.length >= 10) {
+      // Clear any previous error message
+      setState(() {
+        errorText = null;
+      });
+    } else {
+      setState(() {
+        errorText = 'Title must be at least 10 characters';
+      });
+    }
+  }
+}
