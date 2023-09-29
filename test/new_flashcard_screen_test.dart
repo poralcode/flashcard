2eb84865aff86e6b209d5bbb345bd925ac1f6431
Poral <seynalkim@yahoo.com>
2023-09-29 21:19:33 +0800

    Added test case for the input field total number of questions.

--- test/new_flashcard_screen_test.dart
+++ test/new_flashcard_screen_test.dart
@@ -5,20 +5,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flashcard/screens/new_flashcard.dart';
 
 void main() {
-  testWidgets('Check valid Flashcard Title.', (WidgetTester tester) async {
+  testWidgets('Check valid Flashcard Title', (WidgetTester tester) async {
     // Build the NewFlashcard widget
     await tester.pumpWidget(MaterialApp(home: NewFlashcard()));
 
     // Find the TextField by its Key
-    final textFieldKey = Key('title_textfield');
-    expect(find.byKey(textFieldKey), findsOneWidget);
+    final titleTextFieldKey = Key('title_textfield');
+    expect(find.byKey(titleTextFieldKey), findsOneWidget);
 
     // Enter a valid title (e.g., more than 10 characters)
-    await tester.enterText(find.byKey(textFieldKey), 'Valid Title');
+    await tester.enterText(find.byKey(titleTextFieldKey), 'Valid Title');
 
     // Trigger a frame rebuild
     await tester.pump();
@@ -27,12 +26,42 @@ void main() {
     expect(find.text('Title must be at least 10 characters.'), findsNothing);
 
     // Enter an invalid title (less than 10 characters)
-    await tester.enterText(find.byKey(textFieldKey), 'Short');
+    await tester.enterText(find.byKey(titleTextFieldKey), 'Short');
 
     // Trigger a frame rebuild
     await tester.pump();
 
     // Verify that the error text is shown
     expect(find.text('Title must be at least 10 characters.'), findsOneWidget);
+  });
+
+  testWidgets('Check valid Total Number of Questions',
+      (WidgetTester tester) async {
+    // Build the NewFlashcard widget
+    await tester.pumpWidget(MaterialApp(home: NewFlashcard()));
+
+    // Find the TextField by its Key
+    final numberTextFieldKey = Key('number_input');
+    expect(find.byKey(numberTextFieldKey), findsOneWidget);
+
+    // Enter a valid total number (e.g., a positive number)
+    await tester.enterText(find.byKey(numberTextFieldKey), '5');
+
+    // Trigger a frame rebuild
+    await tester.pump();
+
+    // Verify that the error text is not shown
+    expect(find.text('Please provide a valid positive number of questions.'),
+        findsNothing);
+
+    // Enter an invalid total number (e.g., a non-positive number)
+    await tester.enterText(find.byKey(numberTextFieldKey), '0');
+
+    // Trigger a frame rebuild
+    await tester.pump();
+
+    // Verify that the error text is shown
+    expect(find.text('Please provide a valid positive number of questions.'),
+        findsOneWidget);
   });
 }
