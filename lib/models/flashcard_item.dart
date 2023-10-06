c43c7094d1b6cc88cf5a9c86fc88fe0022f6a40f
Poral <seynalkim@yahoo.com>
2023-10-06 11:29:16 +0800

    Some changes

--- lib/models/flashcard_item.dart
+++ lib/models/flashcard_item.dart
@@ -1,12 +1,14 @@
 /* This class is the model for the Flashcard Item. */
+import 'package:flashcard/models/question_answer_item.dart';
+
 class FlashcardItem {
   int score = 0;
   int numberOfQuestions = 0;
   int numberOfTextQuestions = 0;
   int numberOfVisualQuestions = 0;
   String flashcardName = 'Flashcard Item';
+  List<QuestionAnswerItem> questionItems = [];
 
   // Constructor
-  FlashcardItem(this.score, this.numberOfQuestions, this.numberOfTextQuestions,
-      this.numberOfVisualQuestions, this.flashcardName);
+  FlashcardItem(this.score, this.numberOfQuestions, this.numberOfTextQuestions, this.numberOfVisualQuestions, this.flashcardName, this.questionItems);
 }
